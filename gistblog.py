1a3264d64a3ca0ce3527f23d89566ff4c34b8b5a
orjanv <orjanv@gmail.com>
2013-03-28 21:32:34 +0000

    Initial commit

---
+++ gistblog.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+ 
+import json, urllib2, requests
+
+#token = "9d70babadc9e7aab450163154f322cae2ab43305"
+#token = " "
+
+choice = 0
+app_name = 'Gist blog CLI app'
+key = ".mytoken.key"
+
+def WriteTokenToFile(token):
+	# Write token to file
+	f = open(key, 'w')
+	f.write(token)
+	print "\nWrote token to file:", token
+	f.close()
+
+def ReadTokenFromFile(token):
+	f = open(key, 'r')
+	for line in f:
+		token = line.strip()
+	f.close()
+
+def login():
+	# Login to Github
+	user = raw_input("Github username: ")
+	password = raw_input("Github password: ")
+	token = GetToken(user, password)
+	WriteTokenToFile(token)
+	return token
+
+def GetToken(user, password):
+	# login to gist.github.com to get a token and write to config file
+
+	# Check there isn't already an auth code for your app:
+    r = requests.get('https://api.github.com/authorizations', auth=(user, password))
+    for auth in r.json():
+        if auth['note'] == app_name \
+            and 'gist' in auth['scopes']:
+            print "Retrieving existing token"
+            #print json.dumps(auth, indent=4)
+            #print auth['token']
+            return auth['token']
+            
+    print "Creating new token"
+    # Otherwise create a new token
+    r = requests.post('https://api.github.com/authorizations',
+        data=json.dumps({
+            'scopes':['gist'],
+            'note':app_name
+            }),
+        headers={'content-type':'application/json'},
+        auth=(user, password)
+    )
+    return r.json()['token']
+
+def ReadGist():
+	req = urllib2.Request("https://api.github.com/users/orjanv/gists")
+	req.add_header('Authorization', 'token ' + token)
+	data = json.load(urllib2.urlopen(req))
+
+	#print data
+	for d in data:
+		print d[u'created_at']
+		print d[u'description']
+		print '\n'
+
+def PostGist():
+
+	input1 = raw_input("Gist Title: ")
+	input2 = raw_input("Gist Ingress: ")
+	input3 = raw_input("Gist Content: ")
+			
+	gist = {"description": input2, "public": "true", "files": {input1: {"content": input3}}}
+	json_data = json.dumps(gist)
+
+	req = urllib2.Request("https://api.github.com/gists")
+	req.add_header('Authorization', 'token ' + token)
+	result = urllib2.urlopen(req, json_data)
+
+	print "\nGist posted"
+
+def GistBlogMenu():
+	loop = 1
+	while loop == 1:
+		print " "
+		print "Gist blog menu"
+		print " "
+		print "1: Add a gist post"
+		print "2: Get all gist posts"
+		print "3: Quit"
+
+		choice = input("Choose from the menu: ")
+		if choice == 1:
+			PostGist()
+		elif choice == 2:
+			ReadGist()
+		elif choice == 3:
+			loop = 0
+
+# Grab token or login to get token
+token = login()
+
+# Call Gist Blog Menu
+GistBlogMenu()
---
+++ readme.md
@@ -0,0 +1,6 @@
+GistBlog
+--------
+
+My first attempt to learn Python resulted in a Gist Blog.
+
+I had seen someone diong a similar thing, but wanted to learn more programming and how to use IO to files and connecting to APIs.
